f3fa6f9912e9870f7fdaf001e3dcc7d308a9d28e
shradh90 <shradhad1996@gmail.com>
2021-06-25 10:20:30 +0530

    list by appending 30 and 70 to 56

--- LinkedList.java
+++ LinkedList.java
@@ -1,29 +1,51 @@
 package LinkedList;
 
+import java.util.Scanner;
+
 public class LinkedList {
 	 static LinkedList linkedList = new LinkedList();
-
+	    static Scanner scanner = new Scanner(System.in);
 	    private INode head;
 	    private INode tail;
+	    private  int size;
 
 	    public LinkedList() {
 	        this.head = null;
 	        this.tail = null;
+	        this.size = 0;
 	    }
-
+	    int size(){
+	        return this.size;
+	    }
 	    public static void main(String[] args) {
 	        linkedList.createLinkedList();
+	        linkedList.printLinkedList();
+	        
+	        System.out.println("Enter the data to linked list");
+	        Integer element = scanner.nextInt();
+	        System.out.println("Enter the position where you want to insert the data ");
+	        int position = scanner.nextInt();
+	        
+	        linkedList.insertNewNode(element,position);
 	        linkedList.printLinkedList();
 	    }
 
 	    // creates linked list
+	    
 	    public void createLinkedList() {
-	        Node<Integer> myFirstNode = new Node<Integer>(56);
-	        Node<Integer> mySecondNode = new Node<Integer>(30);
-	        Node<Integer> myThirdNode = new Node<Integer>(70);
-	        linkedList.append(myFirstNode);
-	        linkedList.append(mySecondNode);
-	        linkedList.append(myThirdNode);
+	        boolean flag = true;
+	        while (flag) {
+	            System.out.println("Enter the data to add in linked list : ");
+	            Integer key = scanner.nextInt();
+	            Node<Integer> newNode = new Node<Integer>(key);
+	            linkedList.append(newNode);
+	            size++;
+	            System.out.println("if you Want to add more elements then Press 1 and Press 0 to exit");
+	            int userChoice = scanner.nextInt();
+	            if(userChoice == 0)
+	                flag = false;
+	        }
+	        System.out.println("Size is : "+size);
 	    }
 
 	    // adds new nodes to linked list
@@ -58,9 +80,36 @@ public class LinkedList {
 	        }
 	    }
 
-	    // prints linked list
-	    public void printLinkedList() {
-	        System.out.println("My nodes : " + head);
+	    // inserting new node at particular position user wants 
+	    public void insertNewNode(Integer element, int position){
+	        
+	        // to verify index value is in range
+	        if(position < 0 || position> size)
+	            System.out.println("Index Number out of range");
+	        else {
+	            Node<Integer> newNode = new Node<Integer>(element);
+	            // if position = 0 then adds element at the first else iterates till position - 1 and inserts element
+	            if(position == 0) {
+	                newNode.setNext(head);
+	                head.setNext(newNode);
+	            } else {
+	                INode currentNode = head;
+	                INode previousNode = null;
+	                int i = 0;
+	                // iterates till position - 1 
+	                while (i < position - 1){
+	                    previousNode = currentNode;
+	                    currentNode = currentNode.getNext();
+	                    i++;
+	                }
+	                newNode.setNext(currentNode);
+	                previousNode.setNext(newNode);
+	            }
+	        }
 	    }
 
-	}
+	    // prints linked list
+	    public void printLinkedList() {
+	        System.out.println("Nodes : " + head);
+	    }
+}
