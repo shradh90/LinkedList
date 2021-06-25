1c8d09869d2884682dad34694998476eeea1c394
shradh90 <84696361+shradh90@users.noreply.github.com>
2021-06-25 14:36:10 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -3,73 +3,34 @@ package LinkedList;
 import java.util.Scanner;
 
 public class LinkedList {
-	 static Scanner input = new Scanner(System.in);
-
+	
 	    public static void main(String[] args) {
-	        LinkedListOperations operations = new LinkedListOperations();
-	        INode headNode = null;
+	    	 LinkedListOperations operations = new LinkedListOperations();
 
-	        Node<Integer> myFirstNode = new Node<>(56);
-	        Node<Integer> mySecondNode = new Node<>(30);
-	        Node<Integer> myThirdNode = new Node<>(70);
+		        Node<Integer> myFirstNode = new Node<>(56);
+		        Node<Integer> mySecondNode = new Node<>(30);
+		        Node<Integer> myThirdNode = new Node<>(70);
+		       
 
-	        boolean flag = true;
-	        while (flag) {
-	            int option = inputMenu();
-	            switch(option) {
-	                case 1:
-	                    operations.addNodeInBeginnig(myFirstNode);
-	                    flag = true;
-	                    break;
-	                case 2:
-	                    operations.addNodeAtEnd(myThirdNode);
-	                    flag = true;
-	                    break;
-	                case 3:
-	                    System.out.println("Enter the data you want to add");
-	                    Integer element = input.nextInt();
-	                    System.out.println("Enter the position where you want to add the data ");
-	                    int position = input.nextInt();
-	                    operations.addNodeInPosition(element, position);
-	                    flag = true;
-	                    break;
-	                case 4:
-	                    operations.deleteNodeFromBeginning();
-	                    flag = true;
-	                    break;
-	                case 5:
-	                    operations.deleteNodeFromLast();
-	                    flag = true;
-	                    break;
-	                case 6:
-	                    operations.displayNode(headNode);
-	                    flag = true;
-	                    break;
-	                case 7:
-	                    operations.searchElement();
-	                    flag = true;
-	                    break;
-	                case 8:
-	                    flag = false;
-	                    break;
+		        operations.addNodeInBeginnig(myFirstNode);
+		        operations.addNodeAtEnd(myThirdNode);
+		        operations.addNodeInMiddle(mySecondNode);
+		        operations.displayNode();
+
+
+                System.out.println("Enter the data you want to add");
+                Scanner input = new Scanner(System.in);
+                Integer element = input.nextInt();
+                System.out.println("Enter the position where you want to add the data ");
+                Scanner input1 = new Scanner(System.in);
+                int position = input1.nextInt();
+                
+                operations.addNodeInPosition(element, position);
+	            operations.displayNode();
+
+	          
+	                   
 	            }
 	        }
-	    }
-	    public static int inputMenu() {
-
-	        System.out.println();
-	        System.out.println("Enter options to perform operations in Linked List");
-	        System.out.println("Press 1 to Add Node in the Beginning");
-	        System.out.println("Press 2 to Add Node at the End");
-	        System.out.println("Press 3 to Add Node in the Between the List");
-	        System.out.println("Press 4 to Delete Node from the Beginning");
-	        System.out.println("Press 5 to Delete Node from the End");
-	        System.out.println("Press 6 to Display the Linked List");
-	        System.out.println("Press 7 to Search Node in the Linked List");
-	        System.out.println("Press 8 to Quit");
-
-	        int value = input.nextInt();
-
-	        return value;
-	    }
-	}
+	    
+	    
